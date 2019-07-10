7d76e72e95fa546fdcf1ee4882def4ae1e3e67cc
Aliaksei Safonau <aliaksei_safonau@epam.com>
2019-07-10 19:08:30 +0300

    Add HomeViewModel

---
+++ Mobile.App.Presentation/ViewModels/HomeViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System.Threading.Tasks;
+
+namespace Mobile.App.Presentation.ViewModels
+{
+    public class HomeViewModel
+    {
+        public Task SomeMethodAsync()
+        {
+            return Task.CompletedTask;
+        }
+    }
+}
